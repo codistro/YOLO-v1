b5eb3361388a6f49d1b6c31486f28c4ed1fbd313
codistro <rajan191455@gmail.com>
2021-06-12 02:48:04 +0530

    Added Training data

--- scripts/dataset.sh
+++ scripts/dataset.sh
@@ -1,4 +1,6 @@
+wget https://pjreddie.com/media/files/train2014.zip
 wget https://pjreddie.com/media/files/val2014.zip
 wget https://pjreddie.com/media/files/coco/labels.tgz
 unzip val2014.zip
+unzip train2014.zip
 tar -xzvf labels.tgz
