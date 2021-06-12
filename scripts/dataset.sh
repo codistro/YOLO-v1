85ef41a2c423ff8319cf455f99ac2cf255e6789c
codistro <rajan191455@gmail.com>
2021-06-12 22:31:37 +0530

    removed -v

--- scripts/dataset.sh
+++ scripts/dataset.sh
@@ -3,4 +3,4 @@ wget https://pjreddie.com/media/files/val2014.zip
 wget https://pjreddie.com/media/files/coco/labels.tgz
 unzip val2014.zip
 unzip train2014.zip
-tar -xzvf labels.tgz
+tar -xzf labels.tgz
