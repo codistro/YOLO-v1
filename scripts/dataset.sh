f5ea4bd169f2a7edb2a0195b1da0f8499f3c6fd8
codistro <rajan191455@gmail.com>
2021-06-15 20:11:48 +0530

    fix

--- scripts/dataset.sh
+++ scripts/dataset.sh
@@ -1,6 +1,6 @@
-wget https://pjreddie.com/media/files/train2014.zip
+#wget https://pjreddie.com/media/files/train2014.zip
 wget https://pjreddie.com/media/files/val2014.zip
 wget https://pjreddie.com/media/files/coco/labels.tgz
 unzip -qq val2014.zip
-unzip -qq train2014.zip
+#unzip -qq train2014.zip
 tar -xzf labels.tgz
